0a968d17804a18dee43bddf81c7d26023ba8fba9
Harish Krishna V <harishkrishna.94@gmail.com>
2014-11-12 14:58:06 +0530

    Uses Vudoria's SDK
    Multiple images, non-square images supported

---
+++ obj/local/armeabi-v7a/objs/ImageTargetsNative/SampleUtils.o.d
@@ -0,0 +1,109 @@
+obj/local/armeabi-v7a/objs/ImageTargetsNative/SampleUtils.o: \
+ jni/SampleUtils.cpp jni/SampleUtils.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/stdio.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/stdint.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/android/log.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/math.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/limits.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/string.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/malloc.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/alloca.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/strings.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/memory.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h
+
+jni/SampleUtils.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/math.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/limits.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/string.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/strings.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/memory.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h:
---
+++ obj/local/armeabi-v7a/objs/ImageTargetsNative/Texture.o.d
@@ -0,0 +1,67 @@
+obj/local/armeabi-v7a/objs/ImageTargetsNative/Texture.o: jni/Texture.cpp \
+ jni/Texture.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/jni.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ jni/SampleUtils.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/stdio.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/stdint.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/android/log.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/string.h \
+ C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/malloc.h
+
+jni/Texture.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/jni.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+jni/SampleUtils.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/string.h:
+
+C:/NVPACK/android-ndk-r10/platforms/android-8/arch-arm/usr/include/malloc.h:
